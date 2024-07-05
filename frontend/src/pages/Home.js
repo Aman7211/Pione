6cdb8f4dd35d96384ee3edaa4bd00351d61b5ab6
Aman7211 <amanrathore9811@gmail.com>
2024-07-05 19:28:43 +0530

    newwww

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -170,49 +170,49 @@ const cards = [
     content: <SkeletonOne />,
     className: "md:col-span-2",
     thumbnail:
-      "https://img.freepik.com/free-photo/antique-camera-lens-wooden-table-indoors-generated-by-ai_188544-32919.jpg?t=st=1719049208~exp=1719052808~hmac=93b6c002e15da0d91ca1b25d71f1781275b8b83b216d994d02aee97fa0f4212f&w=1060"},
+      "https://res.cloudinary.com/dvahmkxd9/image/upload/v1720171374/pioneer%20ventures/WhatsApp_Image_2024-07-05_at_1.45.25_PM_q9avh5.jpg"},
    {
     id: 2,
     content: <SkeletonTwo />,
     className: "col-span-1",
     thumbnail:
-      "https://img.freepik.com/free-photo/scene-construction-site-with-equipment_23-2151317285.jpg?t=st=1719046459~exp=1719050059~hmac=5ecfa80cb078538f9e1579f53d2df1578fa15c7d02dcf0bbd8f5629877eee26e&w=900" },
+      "https://res.cloudinary.com/dvahmkxd9/image/upload/v1720171374/pioneer%20ventures/WhatsApp_Image_2024-07-05_at_1.46.27_PM_novopx.jpg" },
   {
     id: 3,
     content: <SkeletonThree />,
     className: "col-span-1",
     thumbnail:
-      "https://img.freepik.com/free-photo/top-view-eid-al-fitr-celebration-with-delicious-food_23-2151205080.jpg?t=st=1719048021~exp=1719051621~hmac=50fff2a6ef7bcecad5b79f962f4a3c0c84d13fc5c1492bca966722c946081ca3&w=900" },
+      "https://res.cloudinary.com/dvahmkxd9/image/upload/v1720171375/pioneer%20ventures/WhatsApp_Image_2024-07-05_at_1.56.04_PM_klqhah.jpg" },
   {
     id: 4,
     content: <SkeletonFour />,
     className: "md:col-span-1",
     thumbnail:
-      "https://img.freepik.com/free-photo/pills-dark-environment_23-2151438439.jpg?t=st=1719048122~exp=1719051722~hmac=29a905051deb3bc632dd77f69337b71eeeef9459ff6667225af9cb53fb091219&w=1380"},
+      "https://res.cloudinary.com/dvahmkxd9/image/upload/v1720171374/pioneer%20ventures/WhatsApp_Image_2024-07-05_at_2.11.49_PM_zvjsqy.jpg"},
   {
     id: 5,
     content: <SkeletonFive />,
     className: "md:col-span-1",
     thumbnail:
-      "https://img.freepik.com/free-photo/futurism-perspective-digital-nomads-lifestyle_23-2151252447.jpg?t=st=1719048176~exp=1719051776~hmac=cd4e47c350952256a531d06e9f792e1f7bd143538ce823685bcef4fab6c34436&w=996" },
+      "https://res.cloudinary.com/dvahmkxd9/image/upload/v1720171374/pioneer%20ventures/WhatsApp_Image_2024-07-05_at_2.38.22_PM_alqvvx.jpg" },
   {
     id: 6,
     content: <SkeletonSix />,
     className: "md:col-span-1",
     thumbnail:
-      "https://img.freepik.com/free-photo/view-3d-videographer-with-camera_23-2151067045.jpg?t=st=1719048181~exp=1719051781~hmac=f2e826b59039bf53a7cf11825381f66ba8c75baea59362738952f861dd36c267&w=900"},
+      "https://res.cloudinary.com/dvahmkxd9/image/upload/v1720171375/pioneer%20ventures/WhatsApp_Image_2024-07-05_at_2.40.57_PM_zdvjag.jpg"},
   {
     id: 7,
     content: <SkeletonSeven />,
     className: "md:col-span-1",
     thumbnail:
-      "https://img.freepik.com/free-photo/solar-panels-wind-mills-sunset-sustainable-energy-eco-environment_1268-27834.jpg?t=st=1719048334~exp=1719051934~hmac=d8f97ef5eb02dae7ecbd2c235d16630e5c9a6a20c6b71f91057f4007ad9a9416&w=900" },
+      "https://res.cloudinary.com/dvahmkxd9/image/upload/v1720171375/pioneer%20ventures/WhatsApp_Image_2024-07-05_at_2.42.19_PM_x3ivp8.jpg" },
   {
     id: 8,
     content: <SkeletonEight />,
     className: "md:col-span-1",
     thumbnail:
-      "https://img.freepik.com/free-photo/3d-rendering-wooden-house_23-2151264503.jpg?t=st=1719048218~exp=1719051818~hmac=a2e64698de64a835772cb247e72e436772c1d1530f6dbe4bb1c09e4f21a215e9&w=1060"},
+      "https://res.cloudinary.com/dvahmkxd9/image/upload/v1720171374/pioneer%20ventures/WhatsApp_Image_2024-07-05_at_1.52.02_PM_narbac.jpg"},
 ];
   return (
     <>
@@ -291,11 +291,11 @@ Pioneer Ventures is a leading venture capital firm dedicated to supporting visio
     >
   <div className="flex flex-col md:flex-row items-center md:items-start">
       <div className="text-center  my-3 ml-2 md:ml-[10px]">
-        <p className="text-2xl md:text-3xl font-bold mt-24 md:mt-0">Our Approach</p>
+        <p className="text-2xl md:text-3xl font-bold mt-24 md:mt-20">Our Approach</p>
       </div>
       {/* all content of home page */}
       <div className="text-lg text-center font-medium mx-4 md:mx-[110px]">
-        <p className="my-2 md:my-5">
+        <p className="my-2 md:my-5 md:mt-20">
           At <span className="text-red-800">Pioneer Ventures</span> we believe in more than just funding a business
         </p>
         <p>
