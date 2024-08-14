643bedbf19ea593946b4b1dd6a5a726e6bbc146a
Aman7211 <amanrathore9811@gmail.com>
2024-08-14 16:27:12 +0530

    neww1

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -41,7 +41,7 @@ const Contact = () => {
             <p className="text-gray-800 font-medium text-lg md:text-xl">
               Have a question or want to work together? Fill out the form below or send us an email at{" "}
               <Link to="#" className="underline">
-                info@pioneerventures.com 
+                info@pioneerventure.com 
               </Link>
               .
             </p>
